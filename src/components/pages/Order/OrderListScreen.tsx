586dc71981abd5ff0f7c9b9db49fba476d8ac9a9
cong0912 <congntc912@gmail.com>
2025-02-14 16:08:48 +0700

    List Order , order detail

---
+++ src/components/pages/Order/OrderListScreen.tsx
@@ -0,0 +1,116 @@
+import Breadcrumb from "@common/Breadcrum";
+import Title from "@common/Title";
+import UserMenu from "@atom/user/UserMenu";
+import { Container } from "@styles/styles";
+import { breakpoints, defaultTheme } from "@styles/themes/default";
+import { UserContent, UserDashboardWrapper } from "@styles/user";
+import styled from "styled-components";
+import OrderItemList from "./OrderItemList";
+import { useAppDispatch, useAppSelector } from "@redux/hook";
+import { useEffect, useState } from "react";
+import { getAllOrdersByUsers } from "@redux/slices/orderListSlice";
+
+type OrderStatus =
+  | "pendingPayment"
+  | "processing"
+  | "pendingDelivery"
+  | "delivered"
+  | "paid"
+  | "cancelled"
+  | "refunded";
+
+const OrderListScreenWrapper = styled.div`
+  background-color: #f6f6f6;
+  .order-tabs-contents {
+    margin-top: 40px;
+  }
+  .order-tabs-head {
+    min-width: 140px;
+    padding: 12px 0;
+    border-bottom: 3px solid ${defaultTheme.color_whitesmoke};
+
+    &.order-tabs-head-active {
+      border-bottom-color: ${defaultTheme.color_sea_green};
+    }
+
+    @media (max-width: ${breakpoints.lg}) {
+      min-width: 120px;
+    }
+    @media (max-width: ${breakpoints.xs}) {
+      min-width: 80px;
+    }
+  }
+`;
+
+const breadcrumbItems = [
+  { label: "Home", link: "/" },
+  { label: "Order", link: "/order" },
+];
+
+const OrderListScreen = () => {
+  const dispatch = useAppDispatch();
+  const orderData = useAppSelector((state) => state.orderList.orders) || [];
+
+  useEffect(() => {
+    dispatch(getAllOrdersByUsers());
+  }, [dispatch]);
+
+  const [activeTab, setActiveTab] = useState<OrderStatus>("pendingPayment");
+  const handleTabClick = (tab: OrderStatus) => setActiveTab(tab);
+
+  const orderStatusMap: Record<OrderStatus, string> = {
+    pendingPayment: "PENDING_PAYMENT",
+    processing: "PROCESSING",
+    pendingDelivery: "PENDING_DELIVERY",
+    delivered: "DELIVERED",
+    paid: "PAID",
+    cancelled: "CANCELLED",
+    refunded: "REFUNDED",
+  };
+
+  const filteredOrders = orderData.filter((order) => order.status === orderStatusMap[activeTab]);
+
+  return (
+    <OrderListScreenWrapper className="page-py-spacing">
+      <Container>
+        <Breadcrumb items={breadcrumbItems} />
+        <UserDashboardWrapper>
+          <UserMenu />
+          <UserContent>
+            <Title titleText={"My Orders"} />
+            <div className="order-tabs mb-12">
+              <div className="order-tabs-heads">
+                {Object.keys(orderStatusMap).map((key) => (
+                  <button
+                    key={key}
+                    type="button"
+                    className={`order-tabs-head text-xl italic ${activeTab === key ? "order-tabs-head-active" : ""}`}
+                    onClick={() => handleTabClick(key as OrderStatus)}
+                  >
+                    {key === "pendingPayment" && "Chờ thanh toán"}
+                    {key === "processing" && "Đang xử lí"}
+                    {key === "pendingDelivery" && "Chờ giao hàng"}
+                    {key === "delivered" && "Đã giao hàng"}
+                    {key === "paid" && "Đã thanh toán"}
+                    {key === "cancelled" && "Đã hủy"}
+                    {key === "refunded" && "Trả hàng/Hoàn tiền"}
+                  </button>
+                ))}
+              </div>
+
+              <div className="order-tabs-contents">
+                {filteredOrders.length > 0 ? (
+                  <OrderItemList orders={filteredOrders} />
+                ) : (
+                  <p className="text-center text-gray-500">Không có đơn hàng nào.</p>
+                )}
+              </div>
+            </div>
+          </UserContent>
+        </UserDashboardWrapper>
+      </Container>
+    </OrderListScreenWrapper>
+  );
+};
+
+export default OrderListScreen;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,100 +1,112 @@
 // import MainLayout from "@layouts/MainLayout";
 
-import Home from '@components/pages/Home/Home';
-import { ComponentType } from 'react';
-import AuthLayout from '@layouts/AuthLayout';
-import Register from '@components/pages/Register/Register';
-import VerifyAccount from '@components/pages/VerifyAccount/VerifyAccount';
-import Login from '@components/pages/Login/Login';
-import MainLayout from '@layouts/MainLayout';
-import Product from '@components/pages/Product/Product';
-import ProductDetail from '@components/pages/ProductDetail/ProductDetail';
-import CartScreen from '@components/pages/Cart/Cart';
-import ForgotPassword from '@components/pages/ForgotPassword/ForgotPassword';
-import VerifyOTP from '@components/pages/ForgotPassword/VerifyOTP';
-import ResetPassword from '@components/pages/ForgotPassword/ResetPassword';
-import Account from '@components/pages/Account/Account';
-import CheckoutScreen from '@components/pages/Checkout/Checkout';
-import ConfirmScreen from '@components/pages/Checkout/PaymentSuccess';
-import ErrorScreen from '@components/pages/Checkout/PaymentError';
+import Home from "@components/pages/Home/Home";
+import { ComponentType } from "react";
+import AuthLayout from "@layouts/AuthLayout";
+import Register from "@components/pages/Register/Register";
+import VerifyAccount from "@components/pages/VerifyAccount/VerifyAccount";
+import Login from "@components/pages/Login/Login";
+import MainLayout from "@layouts/MainLayout";
+import Product from "@components/pages/Product/Product";
+import ProductDetail from "@components/pages/ProductDetail/ProductDetail";
+import CartScreen from "@components/pages/Cart/Cart";
+import ForgotPassword from "@components/pages/ForgotPassword/ForgotPassword";
+import VerifyOTP from "@components/pages/ForgotPassword/VerifyOTP";
+import ResetPassword from "@components/pages/ForgotPassword/ResetPassword";
+import Account from "@components/pages/Account/Account";
+import CheckoutScreen from "@components/pages/Checkout/Checkout";
+import ConfirmScreen from "@components/pages/Checkout/PaymentSuccess";
+import ErrorScreen from "@components/pages/Checkout/PaymentError";
+import OrderListScreen from "@components/pages/Order/OrderListScreen";
+import OrderDetailScreen from "@components/pages/Order/OrderDetail";
 
 export type RouteType = {
-    path: string;
-    component: ComponentType<any>;
-    layout?: ComponentType<any> | null | undefined;
+  path: string;
+  component: ComponentType<any>;
+  layout?: ComponentType<any> | null | undefined;
 };
 
 const publicRoute: RouteType[] = [
-    {
-        path: '/',
-        component: Home,
-        layout: MainLayout,
-    },
-    {
-        path: '/login',
-        component: Login,
-        layout: AuthLayout,
-    },
-    {
-        path: '/register',
-        component: Register,
-        layout: AuthLayout,
-    },
-    {
-        path: '/verify',
-        component: VerifyAccount,
-        layout: AuthLayout,
-    },
-    {
-        path: '/forgot-password',
-        component: ForgotPassword,
-        layout: AuthLayout,
-    },
-    {
-        path: '/verify-forgot-password',
-        component: VerifyOTP,
-        layout: AuthLayout,
-    },
-    {
-        path: '/reset-password',
-        component: ResetPassword,
-        layout: AuthLayout,
-    },
-    {
-        path: '/cart',
-        component: CartScreen,
-        layout: MainLayout,
-    },
-    {
-        path: '/product',
-        component: Product,
-        layout: MainLayout,
-    },
-    {
-        path: '/product/:id',
-        component: ProductDetail,
-        layout: MainLayout,
-    },
-    {
-        path: '/checkout',
-        component: CheckoutScreen,
-        layout: MainLayout,
-    },
-    {
-        path: '/account',
-        component: Account,
-        layout: MainLayout,
-    },
-    {
-        path: '/paymentSuccess',
-        component: ConfirmScreen,
-        layout: MainLayout,
-    },
-    {
-        path: '/paymentError',
-        component: ErrorScreen,
-        layout: MainLayout,
-    },
+  {
+    path: "/",
+    component: Home,
+    layout: MainLayout,
+  },
+  {
+    path: "/login",
+    component: Login,
+    layout: AuthLayout,
+  },
+  {
+    path: "/register",
+    component: Register,
+    layout: AuthLayout,
+  },
+  {
+    path: "/verify",
+    component: VerifyAccount,
+    layout: AuthLayout,
+  },
+  {
+    path: "/forgot-password",
+    component: ForgotPassword,
+    layout: AuthLayout,
+  },
+  {
+    path: "/verify-forgot-password",
+    component: VerifyOTP,
+    layout: AuthLayout,
+  },
+  {
+    path: "/reset-password",
+    component: ResetPassword,
+    layout: AuthLayout,
+  },
+  {
+    path: "/cart",
+    component: CartScreen,
+    layout: MainLayout,
+  },
+  {
+    path: "/product",
+    component: Product,
+    layout: MainLayout,
+  },
+  {
+    path: "/product/:id",
+    component: ProductDetail,
+    layout: MainLayout,
+  },
+  {
+    path: "/checkout",
+    component: CheckoutScreen,
+    layout: MainLayout,
+  },
+  {
+    path: "/account",
+    component: Account,
+    layout: MainLayout,
+  },
+  {
+    path: "/order",
+    component: OrderListScreen,
+    layout: MainLayout,
+  },
+  {
+    path: "/order-detail/:orderId",
+    component: OrderDetailScreen,
+    layout: MainLayout,
+  },
+  {
+    path: "/paymentSuccess",
+    component: ConfirmScreen,
+    layout: MainLayout,
+  },
+  {
+    path: "/paymentError",
+    component: ErrorScreen,
+    layout: MainLayout,
+  },
 ];
 
 const privateRoute: RouteType[] = [];
